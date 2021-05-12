3636a3b4aa524025d274d1b314203e364b7c4fd9
Jeremy Casey <jeremy8883@gmail.com>
2021-05-13 09:06:28 +1000

    Add lightId to example config

--- config.example.js
+++ config.example.js
@@ -2,4 +2,5 @@ module.exports = {
   // Get this information here: https://developers.meethue.com/develop/get-started-2/
   hostAddress: "http://192.168.0.0/",
   username: "yourhuepasswordhere",
+  lightId: "1",
 }
